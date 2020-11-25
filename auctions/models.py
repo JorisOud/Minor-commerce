b3c5825aa4415f87a5a3ffa3a51f1e1731c9eb47
JorisOud <jorisoud@hotmail.com>
2020-11-25 10:31:09 +0100

    some imporvements from review feedback

--- auctions/models.py
+++ auctions/models.py
@@ -5,8 +5,14 @@ from django.db import models
 class User(AbstractUser):
     watchlist = models.ManyToManyField("Auction", blank=True)
 
+    def __str__(self):
+        return f"{self.username}"
+
 class Category(models.Model):
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.name}"
 
 class Auction(models.Model):
     title = models.CharField(max_length=100)
@@ -19,14 +25,23 @@ class Auction(models.Model):
     is_active = models.BooleanField(default=True)
     won_by = models.ForeignKey(User, on_delete=models.SET_NULL, related_name="won_auctions", null=True, blank=True)
 
+    def __str__(self):
+        return f"{self.title}"
+
 class Bid(models.Model):
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="current_bids")
     auction = models.ForeignKey(Auction, on_delete=models.CASCADE, related_name="auction_bids")
 
+    def __str__(self):
+        return f"Bid made by {self.creator.username} on {self.auction.title}"
+
 class Comment(models.Model):
     comment_content = models.CharField(max_length=1000)
     creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_comments")
     auction = models.ForeignKey(Auction, on_delete=models.CASCADE, related_name="comments")
+
+    def __str__(self):
+        return f"Comment made by {self.creator.username} on {self.auction.title}"
 
 
